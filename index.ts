c192822847a30c3bb30734acdc8f68f668560bfd
Ivan <kovaletsivan@gmail.com>
2022-04-08 17:33:37 +0300

    commit

--- index.ts
+++ index.ts
@@ -1,34 +1,39 @@
+
 // task 1
 
-export type User = {
+ type User = {
   name:string
   age:number
   occupation:string
+  
 };
 
-export const users: User[] = [
+ const users: User[] = [
   {
       name: 'Max Mustermann',
       age: 25,
-      occupation: 'Chimney sweep'
+      occupation: 'Chimney sweep',
+   
   },
   {
       name: 'Kate Müller',
       age: 23,
-      occupation: 'Astronaut'
+      occupation: 'Astronaut',
+     
   }
 ];
 
-export function logPerson(user: User):void {
+function logPerson(user: User):void {
   console.log(` - ${user.name}, ${user.age}`);
 }
 
 console.log('Users:');
 users.forEach(logPerson);
 
-// task 2
 
-interface User {
+task 2
+
+interface Users {
   name: string;
   age: number;
   occupation: string;
@@ -192,9 +197,9 @@ interface Admin {
   role: string;
 }
 
-export type Person = User | Admin;
+ type Person = User | Admin;
 
-export const persons: Person[] = [
+const persons: Person[] = [
   { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
   {
       type: 'admin',
@@ -228,10 +233,10 @@ export const persons: Person[] = [
   }
 ];
 
-export const isAdmin = (person: Person): person is Admin => person.type === 'admin';
-export const isUser = (person: Person): person is User => person.type === 'user';
+ const isAdmin = (person: Person): person is Admin => person.type === 'admin';
+ const isUser = (person: Person): person is User => person.type === 'user';
 
-export function logPerson(person: Person) {
+ function logPerson(person: Person) {
   let additionalInformation = '';
   if (isAdmin(person)) {
       additionalInformation = person.role;
@@ -242,9 +247,9 @@ export function logPerson(person: Person) {
   console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
 }
 
-export function filterUsers(persons: Person[], criteria: Partial<Omit< User, 'type'>>): User[] {
+ function filterUsers(persons: Person[], criteria:Partial<User>): User[] {
   return persons.filter(isUser).filter((user) => {
-      const criteriaKeys = Object.keys(criteria) as (keyof Omit< User, 'type'>)[];
+      const criteriaKeys = Object.keys(criteria) as (keyof User)[];
       return criteriaKeys.every((fieldName) => {
           return user[fieldName] === criteria[fieldName];
       });
@@ -260,5 +265,63 @@ filterUsers(
   }
 ).forEach(logPerson);
 
-task 6
+// task 6
 
+interface User {
+  type: 'user';
+  name: string;
+  age: number;
+  occupation: string;
+}
+
+interface Admin {
+  type: 'admin';
+  name: string;
+  age: number;
+  role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+  { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+  { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+  { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+  { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
+  { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
+  { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
+];
+
+export function logPerson(person: Person) {
+  console.log(
+      ` - ${person.name}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`
+  );
+}
+
+const getObjectKeys = <T>(obj: T) => Object.keys(obj) as (keyof T)[];
+
+export function filterPersons(persons:Person[], personType: 'user', criteria: Partial<User> ):User[];
+export function filterPersons(persons:Person[], personType: 'admin', criteria:Partial<Admin> ):Admin[];
+
+
+export function filterPersons(persons:Person[], personType: string, criteria: Partial<Person> ):Person[] {
+  return persons
+      .filter((person) => person.type === personType)
+      .filter((person) => {
+          let criteriaKeys = getObjectKeys(criteria);
+          return criteriaKeys.every((fieldName) => {
+              return person[fieldName] === criteria[fieldName];
+          });
+      });
+}
+
+export const usersOfAge23 = filterPersons(persons, 'user', { age: 23 });
+export const adminsOfAge23 = filterPersons(persons, 'admin', { age: 23 });
+
+console.log('Users of age 23:');
+usersOfAge23.forEach(logPerson);
+
+console.log();
+
+console.log('Admins of age 23:');
+adminsOfAge23.forEach(logPerson);
